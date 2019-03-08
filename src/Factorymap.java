0ab1b39003d7a3d35a2f8371cb43e963ff54c2c9
AbrilPal <pal18198@uvg.edu.gt>
2019-03-07 18:00:04 -0600

    FactoryMap

--- src/Factorymap.java
+++ src/Factorymap.java
@@ -1,2 +1,25 @@
+/**
+ * Clase Factory que crea los tres tipos de map segun el usuario indique.
+ * @author Abril Palencia y Roberto Castillo
+ * @since 07/03/2019
+ */
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.LinkedHashMap;
+
 public class Factorymap {
+    public Map crear(int op) {
+        //primera opcion TreeMap
+        if (op == 1) {
+            return new TreeMap();
+        // segunda opcion HashMap
+        } else if (op == 2) {
+            return new HashMap();
+        //tercera opcion LinkedHasgMap
+        } else{
+            return new LinkedHashMap();
+        }
+    }
 }
